6d5832c849e1b00e4e9203f818bf6d18bc3e0d74
hzoi-wildrage <wangxiaohui0821@gmail.com>
2019-10-28 20:08:06 +0800

    Upd

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var app = express();
 
 app.use(express.static('public'));
 
-var server = app.listen(3002, function () {
+var server = app.listen(3000, '0.0.0.0', function () {
     var host = server.address().address;
     var port = server.address().port;
     console.log("Server listen at http://localhost:%d", port);
