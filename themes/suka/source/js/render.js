6df797919dede6fa43ad5fe453878f3159caac02
hzoi-wildrage <wangxiaohui0821@gmail.com>
2019-10-30 10:05:55 +0800

    Upd

---
+++ themes/suka/source/js/render.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function() {
+    renderMathInElement(
+        document.body,
+        {
+            delimiters: [
+                {left: "$$", right: "$$", display: true},
+                {left: "\\[", right: "\\]", display: true},
+                {left: "$", right: "$", display: false},
+                {left: "\\(", right: "\\)", display: false}
+            ]
+        }
+    );
+});
